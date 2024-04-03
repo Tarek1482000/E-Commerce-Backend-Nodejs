6b893cd91a36297b19da038fa8ccd317e8a81171
Tarek Ahmed Abd El Wahab Shafei <115722766+Tarek1482000@users.noreply.github.com>
2024-04-03 17:13:45 +0200

    Update generateJWT.js

--- utils/generateJWT.js
+++ utils/generateJWT.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (payload) => {
   const token = await jwt.sign(payload, process.env.JWT_SECRET_KEY, {
-    expiresIn: "5h",
+    
   });
   return token;
 };
